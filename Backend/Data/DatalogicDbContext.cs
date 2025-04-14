d930ab2c4e536208c3b869f73e6fa3ceddeeb8d2
Nelson Pino <devsoftware.etiquetas@gmail.com>
2025-04-14 11:55:19 -0400

    Configuacion del Entityframework y creacion de la base de datos

---
+++ Backend/Data/DatalogicDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Model;
+
+namespace Backend.Data
+{
+    public class DatalogicDbContext(DbContextOptions<DatalogicDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Productos { get; set; }
+    }
+}
---
+++ Shared/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public double price  { get; set; }
+        public int Quantity { get; set; }
+        public string Location { get; set; } = null!;
+    }
+}
